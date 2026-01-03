ce33a681a9510985233e35281e8fac061ccb0ef0
scooter861013-andris <scooter861013@gmail.com>
2026-01-03 15:50:18 +0100

    Update idokep_scrape.py

--- scripts/idokep_scrape.py
+++ scripts/idokep_scrape.py
@@ -109,9 +109,8 @@ for card in cards:
     icon = None
     alert = None
 
-    # --- FIGYELMEZTETÉS VAN-E? ---
     alert_icon = card.select_one(".forecast-alert-icon")
-    if alert_icon:
+if alert_icon:
     alert = None
 
     # popover tartalom kinyerése
