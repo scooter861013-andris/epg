6f128e33f68f26a2da0a4ce4b6c9872757a24f55
scooter861013-andris <scooter861013@gmail.com>
2026-07-31 16:44:46 +0200

    Update idokep_scrape.py

--- scripts/idokep_scrape.py
+++ scripts/idokep_scrape.py
@@ -282,8 +282,8 @@ data = {
     "location": LOCATION,
     "fronthatas": fronthatas,
     "focim": focim,
-    "updated": (
-        old_data.get("updated")
+    "utoljára változás az időképről": (
+        old_data.get("utoljára változás az időképről")
         if old_data
         and old_data.get("current") == {
             "temperature": current_temp,
